bf1fec49fd6a55303da4b5b27904d0b6f1b3ba1b
iMaX Rider <kwampien@gmail.com>
2022-08-31 00:50:50 +0700

    Delete AppConfig.kt

--- app/src/main/java/com/imax/testapplication/config/AppConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.imax.testapplication.config
-
-object AppConfig {
-    const val API_ENDPOINT = "https://api.openweathermap.org/"
-    const val APP_ID = "TOKEN" //Generate from openweathermap
-
-}
