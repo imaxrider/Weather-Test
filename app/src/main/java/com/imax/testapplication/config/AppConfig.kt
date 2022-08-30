22aa64442da010e7d5a289f0c79e18fe09fbeff1
iMaX Rider <kwampien@gmail.com>
2022-08-30 21:14:48 +0700

    Update AppConfig.kt

--- app/src/main/java/com/imax/testapplication/config/AppConfig.kt
+++ app/src/main/java/com/imax/testapplication/config/AppConfig.kt
@@ -2,6 +2,6 @@ package com.imax.testapplication.config
 
 object AppConfig {
     const val API_ENDPOINT = "https://api.openweathermap.org/"
-    const val APP_ID = "75d206444c0de4e279e0d85a96335fbe"
+    const val APP_ID = "TOKEN" //Generate from openweathermap
 
 }
